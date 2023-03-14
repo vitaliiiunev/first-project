13765fe15598f2f9198bc942c6c25d07a2732206
vitalii_iunev <vetale4ik@gmail.com>
2023-03-14 17:27:45 +0300

    заменна int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {200,5,299};
+        long[] sales = {200,5,299};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
     }
